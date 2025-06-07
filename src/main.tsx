9e4c4666dc9d82641f0e5a05486bc2f67d0c3773
Uros Mitic <um.uros20@gmail.com>
2025-06-07 22:36:59 +0200

    ovo

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 /**
- * @type {import('react-dom/client').Root}
+ * @type {import('react-dom/client').Root} 
  */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
